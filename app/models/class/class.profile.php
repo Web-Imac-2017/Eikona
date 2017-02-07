c7b2cc8efbab8a44e53b058839af8008c0972b7e
boisier <valentin@dufois.fr>
2017-02-07 21:00:15 +0100

    Adding more definitions

--- app/models/class/class.profile.php
+++ app/models/class/class.profile.php
@@ -8,10 +8,11 @@ class Profile
         $profileID;
     }
     
+    //Reteiving informations
     public function getID() {}
     
+    public function getPic() {}
     public function getName() {}
-    
     public function getDesc() {}
     
     public function getViews() {}
@@ -19,5 +20,16 @@ class Profile
     public function isPrivate() {}
     
     public function getOwner($returnID = false) {}
+    public function getPosts($limit = 0) {}
+    
+    //updating informations
+    public function updateName($newName) {}
+    public function updateDesc($newDesc) {}
+    public function updatePic($newPic) {}
+    
+    public function addView($nbr = 1) {}
+    
+    public function setPrivate() {}
+    public function setPublic() {}
 }
 ?>
