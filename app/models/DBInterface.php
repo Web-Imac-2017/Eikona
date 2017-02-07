dd91cf3021ece965a202d1422891e6f7655d1f68
Florian <zobeleflorian@gmail.com>
2017-02-07 13:32:58 +0100

    création de la classe DBInterface - comm avec la bdd

---
+++ app/models/DBInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+class DBInterface{
+
+	private $cnx;
+
+	public function __construct(){
+		$this->cnx = new PDO("mysql:dbname=roger;host=localhost;charset=utf8", "root", "") or die("connexion à la bdd impossible");
+	}
+
+}
