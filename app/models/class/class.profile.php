c7fc17bddbb98cae1b6e7ae06c013fb63bbee0db
boisier <valentin@dufois.fr>
2017-02-07 19:27:10 +0100

    Add firts prototypes for Profile class

---
+++ app/models/class/class.profile.php
@@ -0,0 +1,23 @@
+<?php
+class Profile
+{
+    private $pID; //ID of the profile
+    
+    public function __construct($profileID)
+    {
+        $profileID;
+    }
+    
+    public function getID() {}
+    
+    public function getName() {}
+    
+    public function getDesc() {}
+    
+    public function getViews() {}
+    
+    public function isPrivate() {}
+    
+    public function getOwner($returnID = false) {}
+}
+?>
