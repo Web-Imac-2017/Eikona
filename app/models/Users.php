6e09d5b538d54c1e5ff001d8fa565036c7758077
Florian <zobeleflorian@gmail.com>
2017-02-07 13:33:24 +0100

    création de la classe Users

---
+++ app/models/Users.php
@@ -0,0 +1,34 @@
+<?php
+
+class Users{
+
+	private $id;
+	private $name;
+	private $email;
+	private $passwd;
+
+	public function __construct($_id, $_name, $_email, $_passwd){
+		$this->id = $_id;
+		$this->name = $_name;
+		$this->email = $_email;
+		$this->passwd = $_passwd;
+	}	
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+
+	public function getPasswd(){
+		return $this->passwd;
+	}
+
+}
+
