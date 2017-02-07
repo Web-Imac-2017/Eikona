5aee24d3add19d102547b1270df2b015b2223d80
boisier <valentin@dufois.fr>
2017-02-07 19:31:35 +0100

    Ajout d'une proposition de fonciton pour se connecter à la BDD

--- app/models/DBInterface.php
+++ app/models/DBInterface.php
@@ -7,5 +7,14 @@ class DBInterface{
 	public function __construct(){
 		$this->cnx = new PDO("mysql:dbname=roger;host=localhost;charset=utf8", "root", "") or die("connexion à la bdd impossible");
 	}
+    
+    //Database query handler suggestion - need to be completed with more security
+    public function request($sql, $vars = [])
+    {
+        $stmt = $cnx->prepare($sql);
+        $stmt->execute($vars);
+        
+        return $stmt;
+    }
 
 }
