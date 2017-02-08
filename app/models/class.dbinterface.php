ab1719a890104d9ed6fa329d3328d6550474d339
boisier <valentin@dufois.fr>
2017-02-08 15:34:37 +0100

    Update password for mamp

--- app/models/class.dbinterface.php
+++ app/models/class.dbinterface.php
@@ -5,6 +5,6 @@ class DBInterface{
 	protected $cnx;
 
 	public function __construct(){
-		$this->cnx = new PDO("mysql:dbname=roger;host=localhost;charset=utf8", "root", "") or die("connexion à la bdd impossible");
+		$this->cnx = new PDO("mysql:dbname=roger;host=localhost;charset=utf8", "root", "root") or die("connexion à la bdd impossible");
 	}
 }
