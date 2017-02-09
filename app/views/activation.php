f5a296ef671bc509c06586461d1a967e7a879f4c
Florian <zobeleflorian@gmail.com>
2017-02-09 18:36:22 +0100

    formulaire d'activation dans le mail

--- app/views/activation.php
+++ app/views/activation.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Page d'activations</title>
+		<title>Page d'activation</title>
 		<meta charset="utf-8">
 	</head>
 
 	<body>
 
 		<h1>Page d'activation</h1>
-		<form method="POST">
-			<input type="hidden" name="id" value="<?php echo $_GET['id']; ?>" required>
-			<input type="hidden" name="key" value="<?php echo $_GET['key']; ?>" required>
-			<input type="submit" value="ACTIVER">
-		</form>
 
 		<?php if(isset($log)): ?>
 		<h4><?php echo $log; ?></h4> 
