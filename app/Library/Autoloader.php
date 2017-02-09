a2dc3ca301dbe815c9af0fc997a0e7012f09f043
boisier <valentin@dufois.fr>
2017-02-09 20:11:22 +0100

    Add possibility for static Loads and update paths

--- app/Library/Autoloader.php
+++ app/Library/Autoloader.php
@@ -10,6 +10,7 @@ class Autoloader
     public static function register()
     {
         spl_autoload_register(array(__CLASS__, 'autoload'));
+        $this->staticLoads();
     }
 
     /**
@@ -20,12 +21,20 @@ class Autoloader
     {
         $class = strtolower($class);
 
-        if(file_exists("models/class.".strtolower($class).".php"))
-            require_once "models/class.".strtolower($class).".php";
-        else if(file_exists("models/class.".$class.".php"))
-            require_once "models/class.".$class.".php";
-        else if(file_exists("controllers/".$class.".php"))
-            require_once "controllers/".$class.".php";
+        if(file_exists("models/.".$class."Model.php"))
+            require_once "models/.".$class."Model.php";
+        else if(file_exists("views/class.".$class."View.php"))
+            require_once "models/views.".$class."View.php";
+        else if(file_exists("controllers/".$class."Controller.php"))
+            require_once "controllers/".$class."Controller.php";
         else return;
+    }
+
+    /**
+     * Include all others files that may be needed
+     */
+    static function staticLoads()
+    {
+        require_once "../DBInterface.php";
     }
 }
