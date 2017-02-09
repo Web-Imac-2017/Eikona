eca41fa39523c061b55006b8cedd50b793dc1fc2
boisier <valentin@dufois.fr>
2017-02-09 19:39:52 +0100

    Add the parseURI function

--- app/controllers/FrontController.php
+++ app/controllers/FrontController.php
@@ -7,7 +7,7 @@ class FrontController
     /**
      * Set default values
      */
-    protected $controllers = "index";
+    protected $controller  = "index";
     protected $action      = "index";
     protected $params      = [];
 
@@ -30,7 +30,30 @@ class FrontController
         else { }
     }
 
+    /**
+     * Parse the URI and set $controller, $action, $params acordingly
+     */
     protected function parseURI()
     {
+        //Clean up URL
+        $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+
+        //Remove basepath
+        if(strpos($path, $this->basePath) == 0)
+        {
+            $path = substr($path, strlen($this->basePath));
+        }
+
+        //Get all given variables
+        @list($controller, $action, $params) = explode("/", $path, 3);
+
+        if(isset($controller))
+            $this->setController($controller);
+
+        if(isset($action))
+            $this->setAction($action);
+
+        if(isset($params))
+            $this->setParams(explode("/", $params));
     }
 }
