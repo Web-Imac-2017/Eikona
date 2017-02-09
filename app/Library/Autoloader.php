4ab25ea8f2c5b86a44733ffa1c08e990f3c16321
boisier <valentin@dufois.fr>
2017-02-09 18:47:52 +0100

    Ajout de l'autoloader

---
+++ app/Library/Autoloader.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class Autoloader
+ */
+class Autoloader
+{
+    /**
+     * Register Autoloader handler
+     */
+    public static function register()
+    {
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+    /**
+     * Include the required class file
+     * @param $class string Name of the class to load
+     */
+    static function autoload($class)
+    {
+        $class = strtolower($class);
+
+        if(file_exists("models/class.".$class.".php"))
+            require_once "models/class.".$class.".php";
+        else throw new Exception("The class ".$class." could not be loaded : File not found");
+    }
+}
