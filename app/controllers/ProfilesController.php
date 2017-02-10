e8048f99bcfa52feb4b15c716e6ee9cb95826d9b
boisier <valentin@dufois.fr>
2017-02-10 22:28:29 +0100

    More Prototypes

--- app/controllers/ProfilesController.php
+++ app/controllers/ProfilesController.php
@@ -5,7 +5,10 @@ class ProfilesController
     /**
      * Create a new profile for the current user
      */
-    public function create(){ }
+    public function create()
+    {
+
+    }
 
     /**
      * Return the profile sheet to display
@@ -15,20 +18,62 @@ class ProfilesController
     public function display($profileID){ }
 
     /**
-     * Return the specified element about the profile
+     * Return the description of the specified profile
      *
      * @param $profileID ID of the profile
-     * @param $whatToGet field to retrieve
      */
-    public function get($profileID, $whatToGet){ }
+    public function name($profileID){ }
+
+    /**
+     * Return the description of the specified profile
+     *
+     * @param $profileID ID of the profile
+     */
+    public function description($profileID){ }
+
+    /**
+     * Return the link to the profile picture of the specified profile
+     *
+     * @param $profileID ID of the profile
+     */
+    public function picture($profileID){ }
+
+    /**
+     * Return the number of views of the specified profile
+     *
+     * @param $profileID ID of the profile
+     */
+    public function views($profileID){ }
+
+    /**
+     * Return the number of views of the specified profile
+     *
+     * @param $profileID ID of the profile
+     */
+    public function isPrivate($profileID){ }
+
+    /**
+     * Return the number of views of the specified profile
+     *
+     * @param $profileID ID of the profile
+     * @param $limit number of posts to return
+     */
+    public function posts($profileID, $limit = 987654321){ }
 
     /**
      * Update the specified element of the profile
      *
+     * @param $field Field to be updated
      * @param $profileID ID of the profile
-     * @param $whatToSet Field to be updated
      */
-    public function update($profileID, $whatToSet){ }
+    public function update($field, $profileID){ }
+    /**
+     * Increment by one (or more) the view counter of the specified profile
+     *
+     * @param $porfileID ID of the profile
+     * @param $nbr Number of view to add.
+     */
+    public function addView($profileID, $nbr = 1){ }
 
     /**
      * Delete the specified profile and all its dependecies
