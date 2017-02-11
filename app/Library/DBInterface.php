f060b0ef4d102fab93f794cfc9eb4f66ad6bcb38
boisier <valentin@dufois.fr>
2017-02-11 16:16:00 +0100

    Use UTF-8

--- app/Library/DBInterface.php
+++ app/Library/DBInterface.php
@@ -6,5 +6,6 @@ class DBInterface{
 
 	public function __construct(){
 		$this->cnx = new PDO("mysql:dbname=roger;host=localhost;charset=utf8", "root", "root") or die("connexion à la bdd impossible");
+        $this->cnx->exec("SET CHARACTER SET utf8");
 	}
 }
