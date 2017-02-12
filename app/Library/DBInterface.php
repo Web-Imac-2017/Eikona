1099e3db095da417aea4bd14e9ea4a12141af16e
boisier <valentin@dufois.fr>
2017-02-12 16:17:13 +0100

    Add __call methods to handle wrong calls to models

--- app/Library/DBInterface.php
+++ app/Library/DBInterface.php
@@ -8,4 +8,27 @@ class DBInterface{
 		$this->cnx = new PDO("mysql:dbname=roger;host=localhost;charset=utf8", "root", "root") or die("connexion à la bdd impossible");
         $this->cnx->exec("SET CHARACTER SET utf8");
 	}
+
+
+
+
+    /**
+     * Hnadle call to an undefined method.
+     */
+    public function __call($method, $args)
+    {
+        echo "Unknown method <strong>".$method."</strong><br>";
+        echo "Supported methods :<br><ul>";
+
+        $class_methods = get_class_methods(get_called_class());
+
+        foreach ($class_methods as $method_name)
+        {
+            echo "<li>".$method_name."</li>";
+        }
+
+        echo "</ul>";
+
+        return false;
+    }
 }
