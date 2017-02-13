7a15e972f97589e3c99248afbc0b026cf43936be
boisier <valentin@dufois.fr>
2017-02-13 18:47:38 +0100

    Adding isAuthorized

---
+++ app/Library/isAuthorized.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * isAuthorized - Functions handling authorizations
+ */
+class isAuthorized
+{
+    /***** Global verifications *****/
+    static public function isUser() {}
+    static public function isModerator() {}
+    static public function isAdmin() {}
+
+    /***** Profiles verifications *****/
+    static public function canUpdateProfile() {}
+    static public function canDeleteProfile() {}
+}
