95f460bf27db9bcc09aa1d741529d9ae4b4b2914
Florian <zobeleflorian@gmail.com>
2017-02-15 16:48:39 +0100

    ajout du model UserModel.php

--- app/models/AuthModel.php
+++ app/models/AuthModel.php
@@ -14,11 +14,16 @@ class AuthModel extends DBInterface{
 	/**
 	 * Vérifie si l'utilisateur est unique
 	 * @param  text $email user_email
-	 * @return [type]        [description]
+	 * @return boolean	    true / false
 	 */
 	public function uniqueUser($email)	
 	{
-		
+		$stmt = $this->cnx->prepare("
+			SELECT user_email FROM users
+			WHERE user_email = :email");	
+		$stmt->execute([":email" => $email]);
+
+		return ($stmt->rowCount == 0) ? true : false;
 	}
 
 	/**
@@ -27,10 +32,22 @@ class AuthModel extends DBInterface{
 	 * @param text $email  user_email
 	 * @param text $passwd user_passwd
 	 * @param time $time   time()
+	 * @return int         id ajouté dans la table
 	 */
 	public function addUser($name, $email, $passwd, $time)
 	{
+		// cryptage du mot de passe en sha256
+		$pwd = hash('sha256', $passwd);
 
+		$stmt = $this->cnx->prepare("
+			INSERT INTO users (user_name, user_email, user_passwd, user_register_time)
+			VALUES (:name, :email, :pwd, :time)");
+		$stmt->execute([":name"  => $name,
+			            ":email" => $email,
+			            ":pwd"   => $pwd,
+			            ":time"  => $time]);
+
+		return $this->cnx->lastInsertId();
 	}
 
 	/**
@@ -53,21 +70,31 @@ class AuthModel extends DBInterface{
 	 * Vérifie si l'utilisateur existe bien dans la databse
 	 * @param  int $id  user_id
 	 * @param  int  $key clé_cryptée
-	 * @return [type]      [description]
+	 * @return boolean   true / false
 	 */
 	public function checkUserExists($id, $key)
 	{
+		$stmt = $this->cnx->prepare("
+			SELECT user_id, user_register_time, user_activated FROM users
+			WHERE :id = user_id
+			AND :key = sha1(user_register_time)");
+		$stmt->execute([":id"  => $id,
+			            ":key" => $key]);
 
+		return ($stmt->rowCount() == 1) ? true : false;
 	}
 
 	/**
 	 * Active le compte de l'utilisateeur
-	 * @param  int $id user_id
+	 * @param  int $id user_id 	
 	 * @return [type]     [description]
 	 */
 	public function updateUserActivated($id)
-	{
-
+	{	
+		$stmt = $this->cnx->prepare("
+			UPDATE users SET user_activated = 1
+			WHERE :id= user_id");
+		$stmt->execute([":id" => $id]);	
 	}
 
 
@@ -78,11 +105,24 @@ class AuthModel extends DBInterface{
 	/**
 	 * Connexion (ou non) de l'utilisateur
 	 * @param  text $email  user_email
-	 * @param  text $passwd user_passwds
+	 * @param  text $passwd user_passwd
 	 * @return [type]         [description]
 	 */
 	public function checkAuth($email, $passwd)
 	{
+		//cryptage du passwd
+		$pwd = hash('sha256', $passwd);
+		$stmt = $this->cnx->prepare("
+			SELECT * FROM  users
+			WHERE :email = user_email
+			AND :pwd = user_passwd");
+		$stmt->execute([":email"  => $email,
+			            ":passwd" => $pwd]);
 
+		//S'il y a une seule réponse (donc un seul user)
+		if($res->rowCount() == 1){
+			$u = $stmt->fetch(PDO::FETCH_ASSOC);
+			
+		}
 	}
 }
---
+++ app/models/UserModel.php
@@ -0,0 +1,14 @@
+<?php
+
+class User extends DBInterface{
+
+	private $pID;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	
+
+}
