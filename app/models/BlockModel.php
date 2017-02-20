d9fd44dd041ba85019bc8092f06c286555286250
Patrizio <adripatriz@gmail.com>
2017-02-20 16:53:43 +0100

    Add unblock function to blockModel

--- app/models/BlockModel.php
+++ app/models/BlockModel.php
@@ -7,12 +7,8 @@ class BlockModel extends DBInterface{
 		parent::__construct();
 	}
 
-	/***********************/
-	/***** INSCRIPTION *****/
-	/***********************/
-
 	/**
-	 * Vérifie si l'utilisateur est unique
+	 * Indique dans la BDD que le premier utilisateur fourni bloque le second.
 	 * @param  int $blocker_id blocker_id
 	 * @param  int $blocked_id blocked_id
 	 * @param  time $time time()
@@ -21,9 +17,24 @@ class BlockModel extends DBInterface{
 	public function blockUser($blocker_id, $blocked_id, $time)
 	{
 		$stmt = $this->cnx->prepare("INSERT INTO blocked (blocked_id, blocker_id, block_time) VALUES (:blocker_id, :blocked_id, :time)");
-		$stmt->execute([":blocker" => $blocker_id,
-                        ":blocked" => $blocked_id,
+		$stmt->execute([":blocker_id" => $blocker_id,
+                        ":blocked_id" => $blocked_id,
                         ":time" => $time]);
+
+		return ($stmt->fetchColumn() == 0) ? true : false;
+	}
+
+	/**
+	 * Indique dans la BDD que le premier utilisateur fourni NE bloque PLUS le second.
+	 * @param  int $blocker_id blocker_id
+	 * @param  int $blocked_id blocked_id
+	 * @return boolean	       true / false
+	 */
+	public function unblockUser($blocker_id, $blocked_id)
+	{
+		$stmt = $this->cnx->prepare("DELETE FROM blocked WHERE blocker_id = :blocker_id AND blocker_id = :blocked_id");
+		$stmt->execute([":blocker_id" => $blocker_id,
+                        ":blocked_id" => $blocked_id]);
 
 		return ($stmt->fetchColumn() == 0) ? true : false;
 	}
