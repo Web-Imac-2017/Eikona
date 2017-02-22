53bc75e4c349eca58ea54fc6ffa5fb7054ba6cd1
florian <zobeleflorian@gmail.com>
2017-02-22 22:26:11 +0100

    checkout user

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,146 @@
+<?php
+
+class AuthController{
+
+	private $model;	
+
+	public function __construct()
+	{
+		$this->model = new AuthModel();
+	}
+
+	/**
+	 * Inscription de l'user
+	 * @return Response JSON
+	 */
+	public function register()
+	{
+		$resp = new Response();
+
+		//Si formulaire rempli
+		if(!empty($_POST['user_name']) &&
+		   !empty($_POST['user_email']) &&
+		   !empty($_POST['user_passwd']) &&
+		   !empty($_POST['user_passwd_confirm'])){
+
+			//Si passwd == passwd confirm
+			if($_POST['user_passwd'] == $_POST['user_passwd_confirm']){
+				//si user est unique
+				if($this->model->uniqueUser($_POST['user_email'])){
+					//insertion dans la base de données
+					$user_register_time = time();
+					$id = $this->model->addUser(
+						$_POST['user_name'],
+						$_POST['user_email'],
+						$_POST['user_passwd'],
+						$user_register_time);
+					//envoi d'un mail d'activation
+					$this->model->sendMail(
+						$id,
+						$_POST['user_email'],
+						$user_register_time);
+					$resp->setSuccess(201, "user added")
+					     ->bindValue("email", $_POST['user_email'])
+					     ->bindValue("userID", $id);
+				}else{
+					$resp->setFailure(403, "user already exists");
+				}				
+			}else{
+				$resp->setFailure(409, "user_passwd et user_passwd_confirm ne sont pas les mêmes");
+			}
+		}else{
+			$resp->setFailure(400, "tous les champs ne sont pas remplis");
+		}
+		
+		//envoi de la réponse	
+		$resp->send();
+	}
+
+	/**
+	 * Activation du compte
+	 * @return Response JSON
+	 */
+	public function activate()
+	{
+		$resp = new Response();
+
+		if(!empty($_REQUEST['user_id']) && !empty($_REQUEST['user_key'])){
+
+			//Activation du compte
+			$res = $this->model->checkActivation($_REQUEST['user_id'], $_REQUEST['user_key']);
+			//Si l'user existe bien
+			if($res){
+				$this->model->updateUserActivated($_REQUEST['user_id']);
+				$resp->setSuccess(200, "Account activated")
+				     ->bindValue("userID", $_REQUEST['user_id']); 
+			}else{
+				$resp->setFailure(409, "user_id or and user_key do not exist");
+			}
+		}else{
+			$resp->setFailure(400, "tous les champs ne sont pas remplis");
+		} 	
+		
+		//envoi de la réponse
+		$resp->send();
+	}
+
+
+	/**
+	 * Connexion
+	 * @return Response JSON
+	 */
+	public function signIn()
+	{
+		$resp = new Response();
+
+		//si les deux champs de connexion sont remplis
+		if(!empty($_POST['user_email']) &&
+		   !empty($_POST['user_passwd'])){
+
+			$email = $this->model->checkEmail($_POST['user_email']);
+
+			//Si l'user est inscrit dans la bdd
+			if($email){
+				$user = $this->model->checkConnection($_POST['user_email'], $_POST['user_passwd']);
+				//Si la combinaison email /pwd est correcte
+				if($user->getID() != 0){
+					//Si le compte est activé
+					if($user->getActivated()){
+						$resp->setSuccess(200, "user connected")
+						     ->bindValue("userID", $user->getID())
+						     ->bindValue("userEmail", $_POST['user_email']);
+						Session::renewKey();
+						Session::write("userID", $user->getID());
+					}else{
+						$resp->setFailure(401, "account not yet activated");
+					}
+				}else{
+					$resp->setFailure(409, "wrong password");
+				}
+			}else{
+				$resp->setFailure(404, "unknown user");
+			}
+		}else{
+			$resp->setFailure(400, "tous les champs ne sont pas remplis");
+		}
+
+		//envoi de la réponse
+		$resp->send();
+	}
+
+	/**
+	 * Deconnexion
+	 * @return Response JSON
+	 */
+	public function signOut()
+	{		
+		$resp = new Response();
+		$resp->setSuccess(200, "user deconnected")
+			 ->bindValue("id", Session::read("userID"))
+			 ->send();
+
+		Session::renewKey();
+		Session::remove("userID");
+		Session::remove("profileID");
+	}
+}
