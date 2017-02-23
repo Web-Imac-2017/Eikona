e5b69a8b78385643ddf416d22915d89fd542bb00
JeanMicheljaipascasurmonordi <owenmanach@gmail.com>
2017-02-23 17:25:13 +0100

    inscription formulaire html

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <topHeader></topHeader>
+    <inscription></inscription>
     <router-view></router-view>
     <mainFooter></mainFooter>
   </div>
@@ -10,12 +11,14 @@
 
 let topHeader = require('./components/Header.vue')
 let mainFooter = require('./components/mainFooter.vue')
+let inscription = require('./components/Inscription.vue')
 
 export default {
   name: 'app',
   components: {
     topHeader,
-    mainFooter
+    mainFooter,
+    inscription
   }
 
 }
