f1b480c7a675d0168a1f5e2738924bf1b832b1fe
Laurie Greinert <lauriegreinert@gmail.com>
2017-02-28 10:52:09 +0100

    Ajout vérif Tag controller + model + fonction count

--- app/controllers/TagController.php
+++ app/controllers/TagController.php
@@ -2,6 +2,8 @@
 
 class TagController
 {
+	private $model;
+
 	public function __construct()
 	{
 		$this->model = new TagModel();
@@ -9,30 +11,94 @@ class TagController
 
 	/*
 	 * Add a comment
-	 *
+	 * Verify if there is a profile and if it's autorized
+	 * Verify if the post exist
+	 * @param $postID Id of the post where to add the tagName
+	 * @param $tagName Name of the tag to add
 	 */
-	public function add()
+	public function add($postID, $tagName)
+	{
+		//Manque le Sanitize des mots bannis
+		//$tagName = Sanitize::string($tagName);
+		$rsp = new Response();
+
+		$profileID = Session::read("profileID");
+
+		if(!$profileID){
+			$rsp->setFailure(401, "You do not have current profile selected")
+			    ->send();
+			return;
+		}
+
+		if(!isAuthorized::editPost($postID)){
+			$rsp->setFailure(401, "You are not authorized to do this action.")
+			    ->send();
+			return;
+		}
+
+		//Verify if the tagName for this post already exist
+		$result = $this->model->addTag($postID, $tagName);
+
+		if($result == "existAlready")
+		{
+			$rsp->setFailure(401, "This tag already exist for this post.")
+			    ->send();
+			return;
+		}
+
+		$rsp->setSuccess(200, "tag added")
+			->send();
+	}
+
+	/*
+	 * Delete a tag
+	 * Verify if there is a profile and if it's autorized
+	 * Verify if the post exist
+	 * @param $postID Id of the post where there's the tagName
+	 * @param $tagName Name of the tag to remove
+	 */
+	public function delete($postID, $tagName)
 	{
 		$rsp = new Response();
 
 		$profileID = Session::read("profileID");
 
-		//Vérifier avec la vérif créé par Florian
+		if(!$profileID){
+			$rsp->setFailure(401, "You do not have current profile selected")
+			    ->send();
+			return;
+		}
 
-		$rsp->send;
+		if(!isAuthorized::editPost($postID)){
+			$rsp->setFailure(401, "You are not authorized to do this action.")
+			    ->send();
+			return;
+		}
+
+		//Vérifier si l'id du post existe
+		$result = $this->model->deleteTag($postID, $tagName);
+
+		if($result == "notFound")
+		{
+			$rsp->setFailure(400, "not found");
+		} else {
+			$rsp->setSuccess(200, "tag deleted")
+			->bindValue("result", $result);
+		}
+			$rsp->send();
 	}
 
 	/*
-	 * Delete a comment
-	 *
+	 * Number of a tag with this tagName
+	 * @param $tagName name of the tag to find
 	 */
-	public function delete()
+	public function count($tagName)
 	{
-		//Vérifier que l'utilisateur a le droit de faire ça
-	}
+		$nbTag = $this->model->countTag($tagName);
 
-	public function countTag()
-	{
-
+		$rsp = new Response;
+		$rsp->setSuccess(200)
+			->bindValue("nbTag", $nbTag)
+			->send();
 	}
 }
--- app/models/TagModel.php
+++ app/models/TagModel.php
@@ -1,34 +1,83 @@
 <?php
 
-class PostModel extends DBInterface
+class TagModel extends DBInterface
 {
-	private $commentData = null;
 
 	public function __construct()
 	{
 		parent::__construct();
 	}
 
-	public function addTag($postID, $tagName)
+	private function existTag($postID, $tagName)
 	{
-		$tagName = Sanitize::string($tagName);
-
-		$stmt = $this->cnx->prepare("INSERT INTO comments(tag_name, post_id, use_time) VALUES(:tagName, :postID, :useTime) WHERE post_id = :postID");
-		$stmt->execute([":tagName" => $tagName,
-						":postID"  => $postID,
-						":useTime" => time()
- 		]);
-	}
-
-	public function deleteTag()
-	{
-		$stmt = $this->cnx->prepare("DELETE FROM tags WHERE post_id = :postID AND tag_name = :tagName");
-		$stmt->execute([":postID"  => $this->postID,
-					    ":tagName" => $this->tagName
+		// Verify if the tagName exist
+		$stmtVerify = $this->cnx->prepare("SELECT tag_name FROM tags WHERE tag_name = :tagName AND post_id = :postID");
+		$stmtVerify->execute([":postID"  => $postID,
+							  ":tagName" => $tagName
 		]);
+
+		//The tag doesn't exist
+		if($stmtVerify->fetch() == 0)
+		{
+			return false;
+		}
 
 		return true;
 	}
 
-	//countTag($tagName);
+	/*
+	 * Add a comment
+	 * @param $postID Id of the post where to add the tagName
+	 * @param $tagName Name of the tag to add
+	 */
+	public function addTag($postID, $tagName)
+	{
+		if($this->existTag($postID, $tagName))
+		{
+			return "existAlready";
+		}
+
+		$stmt = $this->cnx->prepare("INSERT INTO tags(tag_name, post_id, use_time) VALUES (:tagName, :postID, :useTime)");
+		$stmt->execute([":postID"  => $postID,
+						":tagName" => $tagName,
+						":useTime" => time()
+ 		]);
+
+		return "success";
+	}
+
+	/*
+	 * Delete a tag
+	 * @param $postID Id of the post where there's the tagName
+	 * @param $tagName Name of the tag to remove
+	 */
+	public function deleteTag($postID, $tagName)
+	{
+		if(!$this->existTag($postID, $tagName))
+		{
+			return "notFound";
+		}
+
+		//The tag exist, we delete it
+		$stmt = $this->cnx->prepare("DELETE FROM tags WHERE post_id = :postID AND tag_name = :tagName");
+		$stmt->execute([":postID"  => $postID,
+					    ":tagName" => $tagName
+		]);
+
+		return "success";
+	}
+
+	/*
+	 * Number of a tag with this tagName
+	 * @param $tagName name of the tag to find
+	 */
+	public function countTag($tagName)
+	{
+		$stmt = $this->cnx->prepare("SELECT COUNT(tag_name) FROM tags WHERE tag_name = :tagName");
+		$stmt->execute([":tagName" => $tagName]);
+
+		$nbTag = $stmt->fetchColumn();
+
+		return $nbTag;
+	}
 }
