dd9f243d77b9fdd2d09c6af05faf34432015ba53
boisier <valentin@dufois.fr>
2017-03-01 21:50:32 +0100

    set cnx back to protected

--- app/Library/DBInterface.php
+++ app/Library/DBInterface.php
@@ -2,7 +2,7 @@
 
 class DBInterface{
 
-	public $cnx;
+	protected $cnx;
 
 	public function __construct(){
 		$this->cnx = new PDO("mysql:dbname=roger;host=localhost;charset=utf8", "root", "root") or die("connexion à la bdd impossible");
