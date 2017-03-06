83c6f856df3fb751e83d9db23196da9e93ce138e
JeanMicheljaipascasurmonordi <owenmanach@gmail.com>
2017-03-06 12:49:40 +0100

    post html + ajout de like

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,15 +1,33 @@
 <template>
 	<div id="post">
-		<div id="infoPosteur">{{userName}}</div>
-		<button :click.native="addLike">ajouter un like : {{nbrLike}}</button>
-		<input type="text" class="new-comment" place-holder="Ajouter un commentaire" @keyup.enter="addComment" v-model="newComment">
-		<ul class="comments-list">
-			<li class="comment" v-for="comment in comments">				
-				<label>{{comment.message}}</label>
-			</li>
-		</ul>
+		<md-card class='post'>
+			<md-card-header>
+				<md-avatar>
+		  			<img src="assets/Eiko.png" alt="Avatar">
+				</md-avatar>
+				<div id="infoPosteur">{{userName}}</div>
+				
+			</md-card-header>
 
-
+	 		<md-card-media>
+				<img src="assets/testPhoto.jpg" alt="Photo test">
+	  		</md-card-media>
+	  		<div class="md-title">{{title}}</div>
+		  	<md-card-actions>
+				<md-button @click.native="addLike">J'aime : {{nbrLike}}</md-button>
+				
+			</md-card-actions>
+			<md-card-content>
+				<input type="text" class="new-comment" placeholder="Ajouter un commentaire" @keyup.enter="addComment" v-model="newComment">
+				<ul class="comments-list">
+					<li class="comment" v-for="comment in comments">				
+						<label>{{comment.message}}</label>
+						<md-button :click.native="addLikeComment">J'aime {{comment.nbrLikeComment}}</md-button>
+						<md-button >Repondre</md-button>
+					</li>
+				</ul>
+			</md-card-content>
+		</md-card>
 	
 	</div>
 </template>
@@ -26,6 +44,7 @@
 				nbrLike : 0,
 				nbrComment : 0,
 				nbrJour : 0,
+				title: 'Look at my mustach',
 				comments: [],
 				newComment: ''
 
@@ -38,7 +57,8 @@
 			},
 			addComment(){
 				this.comments.push({
-					message: this.newComment
+					message: this.newComment,
+					nbrLikeComment: 0
 				})
 				this.newComment=""
 			}
@@ -48,5 +68,11 @@
 </script>
 
 <style scoped>
-
+	li{
+		text-decoration: none,
+		
+	}
+	.post{
+		width: 500px;
+	}
 </style>
