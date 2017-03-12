1264b6be6834cdabb934b1e305377483af5fdd32
iReine <irene.blt@free.fr>
2017-03-12 03:42:02 +0100

    page profil avec infos et posts, reste js

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -1,28 +1,39 @@
 <template>
 	<md-layout md-gutter>
-		<infosEditable></infosEditable>
-		<previewsPosts></previewsPosts>
+		<infosEditable v-if="currentProfile"></infosEditable>
+		<informationsProfilAutre v-else-if="!currentUser"></informationsProfilAutre>
+
+		<previewsPosts v-if="!currentProfile"></previewsPosts>
+		<previewsPostsPerso v-else-if="currentProfile"></previewsPostsPerso>
+
+
 	</md-layout>	
 </template>
 
 <script type="text/javascript">
-	import addPost from './addPost.vue'
-	import infosEditable from './infosProfileEditable.vue'
-	import informationsProfilAutre from './Informations-profil.vue'
-	import previewsPosts from './previewsPosts.vue'
+import infosEditable from './infosProfileEditable.vue'
+import informationsProfilAutre from './Informations-profil.vue'
+import previewsPosts from './previewsPosts.vue'
+import previewsPostsPerso from './previewsPostsPerso.vue'
 
-	export default{
-		name: 'pageProfile',
-		components: {
-			addPost,
-			infosEditable,
-			informationsProfilAutre,
-			previewsPosts
+export default{
+	name: 'pageProfile',
+	components: {
+		infosEditable,
+		informationsProfilAutre,
+		previewsPosts,
+		previewsPostsPerso
+	},
+	data () {
+		return {
+			currentProfile: false
 		}
 	}
+}
 	
 </script>
 
 <style type="text/css" scoped>
+
 	
 </style>
