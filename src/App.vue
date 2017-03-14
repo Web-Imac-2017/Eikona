66f0da841a542f5d72402c045f75f2403dea07e1
JeanMicheljaipascasurmonordi <owenmanach@gmail.com>
2017-03-14 21:01:56 +0100

    component modification de post

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <topHeader></topHeader>
-    <post :post="this.post"></post>
-    <ModificationPost :post="this.post"></ModificationPost>
+   
     <router-view></router-view>
     <mainFooter></mainFooter>
   </div>
@@ -11,26 +10,13 @@
 <script>
 import topHeader from './components/Header.vue'
 import mainFooter from './components/MainFooter.vue'
-import post from './components/Post.vue'
-import ModificationPost from './components/ModificationPost.vue'
+
 export default {
   name: 'app',
   components: {
     topHeader,
-    mainFooter,
-    post,
-    ModificationPost
-  },
-  data(){    
-    return{
-      post:{
-        post_id : 1,
-        post_description :'blablabla the description of the century',
-        editedPicture : null,
-        originalPicture : '../assets/testPhoto.jpg',
-        contactPicture : '../assets/testPhoto.jpg'
-      }
-    }
+    mainFooter
   }
+  
 }
 </script>
