ee597c6b04d719c4718e6cc044b2c9e1b8cb962b
boisier <valentin@dufois.fr>
2017-03-16 13:28:00 +0100

    Update default profile picture path

--- database/DB CREATE.sql
+++ database/DB CREATE.sql
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `Roger`.`profiles` (
   `profile_create_time` INT NOT NULL,
   `profile_views` INT NOT NULL DEFAULT 0,
   `profile_private` TINYINT(1) NOT NULL DEFAULT 0,
-  `profile_picture` VARCHAR(150) NOT NULL DEFAULT 'default.jpg',
+  `profile_picture` VARCHAR(150) NOT NULL DEFAULT '/eikona/assets/default.jpg',
   `profile_key` VARCHAR(36) NULL COMMENT '« UUID() » ',
   PRIMARY KEY (`profile_id`, `user_id`),
   INDEX `USER_ID` (`user_id` ASC),
